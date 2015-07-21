d9ebebe4e6b1fdf4c3bf817180899a7f943b3a51
Ralph Gabuat <dajuanenonli@yahoo.com>
2015-07-21 16:11:40 -0700

    Added switching code

---
+++ View Switcher/BlueViewController.h
@@ -0,0 +1,13 @@
+//
+//  BlueViewController.h
+//  View Switcher
+//
+//  Created by Student on 7/21/15.
+//  Copyright (c) 2015 Student_Ralph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BlueViewController : UIViewController
+
+@end
---
+++ View Switcher/YellowViewController.h
@@ -0,0 +1,13 @@
+//
+//  YellowViewController.h
+//  View Switcher
+//
+//  Created by Student on 7/21/15.
+//  Copyright (c) 2015 Student_Ralph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YellowViewController : UIViewController
+
+@end
