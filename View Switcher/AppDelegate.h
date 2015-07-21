c03fdc7af20940e19e0ab081054b9fc6d336823b
Ralph Gabuat <dajuanenonli@yahoo.com>
2015-07-21 10:29:04 -0700

    Initial Commit

---
+++ View Switcher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  View Switcher
+//
+//  Created by Student on 7/21/15.
+//  Copyright (c) 2015 Student_Ralph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ View Switcher/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  View Switcher
+//
+//  Created by Student on 7/21/15.
+//  Copyright (c) 2015 Student_Ralph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
